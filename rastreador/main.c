31cbe0d12073032a7701220b8b4759d3d54ffd12
Marco <marcav92@hotmail.com>
2019-02-16 12:32:02 -0600

    adding read logic to main.c in rastreador

--- rastreador/main.c
+++ rastreador/main.c
@@ -10,7 +10,7 @@
  *
  * Created on February 14, 2019, 8:16 PM
  */
-
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,12 +21,65 @@
  
 int main (int argc, char **argv) 
 {
+    //input logic parameters/variables
+    char ptrace_request_type ="";
+    char *prog_value = NULL;
+    char option="";
+    int parameters_read = 0;
+
+
+    //processing logic parameters/variables
     int ret, status;
     pid_t child_pid; 
  
-    printf ("the main program process ID is %d\n", (int) getpid()); 
- 
- 
+
+    //parameter input logic
+    while ((option = getopt (argc, argv, "v:V:")) != -1 && parameters_read<1){
+      parameters_read++;
+      switch (option)
+        {
+        case 'v':
+          ptrace_request_type = 'v';
+          prog_value = optarg;
+          break;
+        case 'V':
+          ptrace_request_type = 'V';
+          prog_value = optarg;
+          break;
+
+        case '?':
+          if (optopt == 'c')
+            fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+          else if (isprint (optopt))
+            fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+          else
+            fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+            return 1;
+         
+        default:
+          abort ();
+      }
+      
+    }
+
+    //print to debug input parameters/variables
+    printf ("ptrace_request_type = %c, prog_value = %s\n",
+          ptrace_request_type, prog_value);
+
+    //glue logic to assign 
+    //ptrace_request_type assignment to ptrace_request
+    //'v'-> PTRACE_SYSCALL
+    //'V'-> PTRACE OPTION TO NOT HALT tracee process
+    //processing logic could receive prog_value
+
+
+
+    //processing logic
+
+    printf ("the main program process ID is %d\n", (int) getpid());
+
     child_pid = fork(); //duplicate                                                                                                                                                
     if( child_pid == 0 ){
       //child: The return of fork() is zero                                                                                                                                    
